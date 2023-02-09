b040d8663a4964629e9bfc20d8942b3b2d6d988a
Walter J. Silva <walter4web@gmail.com>
2023-02-09 16:58:27 -0300

    feat: add script main.tf

---
+++ Criando-BucketS3-na-AWS/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+        soursource = "hashicorp/aws"
+        version = "≳ 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "proj-practice-terraform-walter"
+}
