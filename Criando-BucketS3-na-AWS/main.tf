26c4bdd728345ad0b21e0dfcb2e24ef99445cb4b
Walter J. Silva <walter4web@gmail.com>
2023-04-05 01:55:00 -0300

    feat: block public access to s3 object

--- Criando-BucketS3-na-AWS/main.tf
+++ Criando-BucketS3-na-AWS/main.tf
@@ -16,3 +16,12 @@ provider "aws" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "proj-practice-terraform-walter"
 }
+
+resource "aws_s3_bucket_public_access_block" "s3_block" {
+bucket = aws_s3_bucket.s3_bucket.id
+
+block_public_acls       = true
+block_public_policy     = true
+ignore_public_acls      = true
+restrict_public_buckets = true
+}
